d5d00671eb912059b5bb4571cc96dd3bc9c493c5
Shvetsman <80446310+Shvetsman@users.noreply.github.com>
2022-08-18 16:16:26 +0300

    Add files via upload

---
+++ floats.go
@@ -0,0 +1,33 @@
+// Пакет datafile предназначен для чтения данных из файлов.
+package datafile
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+// GetFloats читает значение float64 из каждой строки файла.
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
+	file, err := os.Open(fileName)
+	if err != nil {
+		return numbers, err
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			return numbers, err
+		}
+		numbers = append(numbers, number)
+	}
+	err = file.Close()
+	if err != nil {
+		return numbers, err
+	}
+	if scanner.Err() != nil {
+		return numbers, scanner.Err()
+	}
+	return numbers, nil
+}
